166aff7efd186841dbcec22b038f243d90ec31d5
Jar7011 <jareyes1324@gmail.com>
2024-07-08 23:11:14 -0600

    Implemented InvalidMoveException

--- shared/src/main/java/chess/InvalidMoveException.java
+++ shared/src/main/java/chess/InvalidMoveException.java
@@ -5,9 +5,23 @@ package chess;
  */
 public class InvalidMoveException extends Exception {
 
-    public InvalidMoveException() {}
+    public InvalidMoveException() {
+        super();
+    }
 
     public InvalidMoveException(String message) {
         super(message);
+    }
+
+    public InvalidMoveException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidMoveException(Throwable cause) {
+        super(cause);
+    }
+
+    public InvalidMoveException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
