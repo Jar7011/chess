585742ae8e1255d602e45961bf3daad51d32f879
Jar7011 <jareyes1324@gmail.com>
2024-07-24 18:01:38 -0600

    Started doing server; can do user methods

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,19 @@
 package server;
 
+import dataaccess.AuthDAO;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import dataaccess.UserDAO;
+import handlers.UserHandler;
+import service.UserService;
 import spark.*;
 
 public class Server {
+
+    UserDAO user = new MemoryUserDAO();
+    AuthDAO authorization = new MemoryAuthDAO();
+    UserService service = new UserService(user,authorization);
+    UserHandler userHandler = new UserHandler(service);
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -11,6 +22,9 @@ public class Server {
 
         // Register your endpoints and handle exceptions here.
         //Spark.delete("/db", );
+        Spark.post("/user", ((request, response) -> userHandler.registerHandler(request, response)));
+        Spark.post("/session", ((request, response) -> userHandler.loginHandler(request, response)));
+        Spark.delete("/session", ((request, response) -> userHandler.logoutHandler(request, response)));
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
