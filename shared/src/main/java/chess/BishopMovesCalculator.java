fde04499d77f2c752785c1b5dd4ec74e4a3b36f1
Jar7011 <jareyes1324@gmail.com>
2024-07-26 17:59:50 -0600

    Code quality improvements

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -3,33 +3,12 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class BishopMovesCalculator implements PieceMovesCalculator {
+public class BishopMovesCalculator extends MoveFunctionsCalculator {
 
+    private final int[][] possibleDirections = { {1,1}, {1,-1}, {-1,1}, {-1,-1} };
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> possMoves = new ArrayList<>();
-        int[][] possibleDirections = { {1,1}, {1,-1}, {-1,1}, {-1,-1} };
-
-        for (int[] possibleDirection : possibleDirections) {
-            int possNewRow = myPosition.getRow() + possibleDirection[0];
-            int possNewCol = myPosition.getColumn() + possibleDirection[1];
-
-            while ((possNewRow >= 1 && possNewRow <= 8) && (possNewCol >= 1 && possNewCol <= 8)) {
-                ChessPosition possPosition = new ChessPosition(possNewRow, possNewCol);
-                ChessPiece pieceAlreadyThere = board.getPiece(possPosition);
-                if (pieceAlreadyThere == null) {
-                    possMoves.add(new ChessMove(myPosition, possPosition, null));
-                } else {
-                    if (pieceAlreadyThere.getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
-                        possMoves.add(new ChessMove(myPosition, possPosition, null));
-                    }
-                    break;
-                }
-                possNewRow += possibleDirection[0];
-                possNewCol += possibleDirection[1];
-            }
-        }
-        return possMoves;
+        return calculateMovesQRB(board, myPosition, possibleDirections);
     }
 }
---
+++ shared/src/main/java/chess/MoveFunctionsCalculator.java
@@ -0,0 +1,37 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class MoveFunctionsCalculator implements PieceMovesCalculator {
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        return List.of();
+    }
+
+    public Collection<ChessMove> calculateMovesQRB(ChessBoard board, ChessPosition myPosition, int[][] directions) {
+        Collection<ChessMove> possMoves = new ArrayList<>();
+
+        for (int[] direction : directions) {
+            int possNewRow = myPosition.getRow() + direction[0];
+            int possNewCol = myPosition.getColumn() + direction[1];
+
+            while ((possNewRow >= 1 && possNewRow <= 8) && (possNewCol >= 1 && possNewCol <= 8)) {
+                ChessPosition possPosition = new ChessPosition(possNewRow, possNewCol);
+                ChessPiece pieceAlreadyThere = board.getPiece(possPosition);
+                if (pieceAlreadyThere == null) {
+                    possMoves.add(new ChessMove(myPosition, possPosition, null));
+                } else {
+                    if (pieceAlreadyThere.getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                        possMoves.add(new ChessMove(myPosition, possPosition, null));
+                    }
+                    break;
+                }
+                possNewRow += direction[0];
+                possNewCol += direction[1];
+            }
+        }
+        return possMoves;
+    }
+}
