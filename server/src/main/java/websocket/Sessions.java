3777a1d477bfea5460d867449c9ad719655e23c6
Jar7011 <jareyes1324@gmail.com>
2024-08-12 10:55:03 -0600

    Added additional functions to Sessions.java

--- server/src/main/java/websocket/Sessions.java
+++ server/src/main/java/websocket/Sessions.java
@@ -1,7 +1,10 @@
 package websocket;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
 
+import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Sessions {
@@ -31,5 +34,35 @@ public class Sessions {
             game.remove(authToken, session);
             session.close();
         }
+    }
+
+    public void sendMessage(String authToken, int gameID, ServerMessage serverMessage) throws IOException {
+        Session session = sessionsInGame.get(gameID).get(authToken);
+        String message = new Gson().toJson(serverMessage);
+        if (session.isOpen()) {
+            session.getRemote().sendString(message);
+        }
+    }
+
+    private void sendMessageToAllSessions(int gameID, ServerMessage message, String notThisAuth) throws IOException {
+        String messageJSON = new Gson().toJson(message);
+        ConcurrentHashMap<String, Session> relevantSessions = sessionsInGame.get(gameID);
+
+        for (String authToken : relevantSessions.keySet()) {
+            Session session = sessionsInGame.get(gameID).get(authToken);
+            if (session.isOpen()) {
+                if (notThisAuth == null || !authToken.equals(notThisAuth)) {
+                    session.getRemote().sendString(messageJSON);
+                }
+            }
+        }
+    }
+
+    public void broadcastMessage(int gameID, ServerMessage message, String exceptThisAuth) throws IOException {
+        sendMessageToAllSessions(gameID, message, exceptThisAuth);
+    }
+
+    public void broadcastMessageAll(int gameID, ServerMessage message) throws IOException {
+        sendMessageToAllSessions(gameID, message, null);
     }
 }
